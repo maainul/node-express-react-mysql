e9b9344922fb34df735741698c4cb82433329b8a
maainul <mainul080@yahoo.com>
2022-10-17 19:09:03 +0600

    Add Frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+function App() {
+  return (
+     <>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<ProductList />} />
+            <Route path="/add" element={<AddProduct />} />
+            <Route path="/edit/:id" element={<EditProduct />} />
+            {/* <Route path="/add"><AddProduct /></Route>
+            <Route path="/edit/:id" element={<EditProduct />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+     
+  );
+}
+
+export default App;
