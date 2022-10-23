b7647fc1608aa1ccab2e38f7f1449c1114dfb406
maainul <mainul080@yahoo.com>
2022-10-23 07:48:21 +0600

    Add MUI IN UPdate

--- product-frontend/src/components/EditProduct.jsx
+++ product-frontend/src/components/EditProduct.jsx
@@ -1,64 +1,65 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import axios from "axios";
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useParams } from "react-router-dom";
+import { Stack } from "@mui/system";
+import { Button, TextField } from "@mui/material";
 
 const EditProduct = () => {
-    const [title, setTitle] = useState('');
-    const [price, setPrice] = useState('');
-    const history = useNavigate();
-    const { id } = useParams();
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const history = useNavigate();
+  const { id } = useParams();
 
-    const updateProduct = async (e) => {
-        e.preventDefault();
-        await axios.patch(`http://localhost:5000/products/${id}`, {
-            title: title,
-            price: price
-        });
-        history.push("/");
-    }
+  const updateProduct = async (e) => {
+    e.preventDefault();
+    await axios.patch(`http://localhost:5000/products/${id}`, {
+      title: title,
+      price: price,
+    });
+    history.push("/");
+  };
 
-    useEffect(() => {
-        getProductById();
-    }, []);
+  useEffect(() => {
+    getProductById();
+  }, []);
 
-    const getProductById = async () => {
-        const response = await axios.get(`http://localhost:5000/products/${id}`);
-        setTitle(response.data.title);
-        setPrice(response.data.price);
-    }
+  const getProductById = async () => {
+    const response = await axios.get(`http://localhost:5000/products/${id}`);
+    setTitle(response.data.title);
+    setPrice(response.data.price);
+  };
 
-    return (
-        <div>
-            <form onSubmit={updateProduct}>
-                <div className="field">
-                    <label className="label">Title</label>
-                    <input
-                        className="input"
-                        type="text"
-                        placeholder="Title"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                    />
-                </div>
+  return (
+    <div>
+      <Stack spacing={2} sx={{ m: 2 }}>
+        <TextField
+          label="Title"
+          type="text"
+          placeholder="Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
 
-                <div className="field">
-                    <label className="label">Price</label>
-                    <input
-                        className="input"
-                        type="text"
-                        placeholder="Price"
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                    />
-                </div>
+        <TextField
+          label="Price"
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(e) => setPrice(e.target.value)}
+        />
 
-                <div className="field">
-                    <button className="button is-primary">Update</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+        <Button
+          color="primary"
+          variant="contained"
+          size="small"
+          onClick={updateProduct}
+        >
+          Update
+        </Button>
+      </Stack>
+    </div>
+  );
+};
 
-export default EditProduct
+export default EditProduct;
